126e5bfb8a04afba2c260f80bb5ec06f0b96c56b
noe vaz <jonovagu@gmail.com>
2020-07-17 11:29:46 -0500

    [IMP] adding a dictionary to each key (quadrant) in the __QUADRANTS variable so we can have the index of the quadrant and also the vlaue of the quadrant ->  1:{'index':15, 'value':'X'}, etc.

--- board.py
+++ board.py
@@ -10,27 +10,29 @@ _____|_____|_____
      |     |
   7  |  8  |  9
      |     |"""
-        self.__CUADRANTS = {self.__BOARD[i]:i for i in range(len(self.__BOARD)) if self.__BOARD[i].isnumeric() }
+        self.__QUADRANTS = {self.__BOARD[i]:{"index":i, "value":i} for i in range(len(self.__BOARD)) if self.__BOARD[i].isnumeric() }
         # self.__CUADRANTS = {'1': 15, '2': 21, '3': 27, '4': 61, '5': 67, '6': 73, '7': 108, '8': 114, '9': 120}
         # The key represents the cuadrant
         # The value represents its index in the string
+        print(self.__QUADRANTS)
         self.__board = self.__BOARD
-        self.__cuadrants = self.__CUADRANTS
-        print(self.__cuadrants)
+        self.__quadrants = self.__QUADRANTS
 
-    def get_cuadrants(self):
-        return self.__cuadrants
+    def get_quadrants(self):
+        return self.__quadrants
 
     def set_quadrant(self, quadrant, value):
         quadrant = str(quadrant)
-        self.__board = self.__board[:self.__cuadrants[quadrant]] + value + self.__board[self.__cuadrants[quadrant]+1:]
+        self.__board = self.__board[:self.__quadrants[quadrant]["index"]] +\
+             value + self.__board[self.__quadrants[quadrant]["index"]+1:]
+        self.__quadrants[quadrant]["value"] = value
 
     def get_board(self):
       return self.__board
 
-    def set_reset(self):
+    def reset_board(self):
         self.__board = self.__BOARD
-        self.__cuadrants = self.__CUADRANTS
+        self.__quadrants = self.__QUADRANTS
 
 
 if __name__ == "__main__":
@@ -43,5 +45,5 @@ if __name__ == "__main__":
 
     print("#"*17)
 
-    b.set_reset()
+    b.reset_board()
     print(b.get_board())
